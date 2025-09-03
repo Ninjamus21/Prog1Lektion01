ef25ad110e1b92eb31a884be749c4d095bb19924
Nicolai <nicolaiknudsen@outllok.dk>
2025-09-03 08:54:35 +0200

    lidt videre udvikling samt string optimeringstest

--- src/opgaver/Opgaver.java
+++ src/opgaver/Opgaver.java
@@ -1,6 +1,7 @@
 package opgaver;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Opgaver {
     public static void main(String[] args) {
@@ -12,6 +13,13 @@ public class Opgaver {
         //opgave6();
         //opgave7();
         //opgave8();
+        //BrugAfNumericTypeConversions();
+        //Opgave9();
+        //System.out.println("Sum from 1 to 10 is " + sum(1, 10));
+        //System.out.println("Sum from 20 to 37 is " + sum(20, 37));
+        //System.out.println("Sum from 35 to 49 is " + sum(35, 49));
+        int n = 100_000_0; // number of concatenations
+        runTests(n);
     }
 
     static void opgave2() {
@@ -96,12 +104,97 @@ public class Opgaver {
     static void opgave8() {
         // afstand 15 kilometer
         // tid 50,5 minutter
-
+        Scanner input = new Scanner(System.in);
         // udregn hvad gennemsnitshastighed for miles pr time;
 
         // formel
-        double gennemsnitsfart = (15 / 1.6) / (50.5 / 60);
-        System.out.println(gennemsnitsfart);
+        System.out.println("hvor mange kilometer?");
+        double kilometer = input.nextDouble();
+        double gennemsnitsfart = (kilometer / 1.6) / (50.5 / 60);
+        System.out.println(gennemsnitsfart + "Mph");
+    }
+
+    static void BrugAfNumericTypeConversions() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter purchase amount: ");
+        double purchaseAmount = input.nextDouble();
+
+        double tax = purchaseAmount * 0.06;
+        System.out.println("Sales tax is $" + (int) (tax * 100) / 100.0);
+    }
+
+    static void Opgave9() {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            int Number1 = (int) (Math.random() * 10);
+            int Number2 = (int) (Math.random() * 10);
+
+            if (Number1 < Number2) {
+                // eksempel
+                // 0 // 7 // 5
+                // 7 // 7 // 5
+                // 7 // 5 // 5
+                // 7 // ""5"" // ""7""
+                int HolderNumber = Number1;
+                Number1 = Number2;
+                Number2 = HolderNumber;
+            }
+            System.out.println("Hvad er " + Number1 + " - " + Number2 + "?");
+            int answer = input.nextInt();
+
+            if (Number1 - Number2 == answer) {
+                System.out.println("Helt rigtigt, Good Boy!");
+            } else {
+                System.out.println("Nah buddy try again?");
+                System.out.println(Number1 + " - " + Number2 + " skulle være " + (Number1 - Number2));
+                break;
+            }
+        }
+        input.close();
+    }
+
+    public static int sum(int i1, int i2){
+        int result = 0;
+        for (int i = i1; i <= i2; i++){
+            result += i;
+            System.out.println(result);
+        }
+        return result;
+    }
+
+    public static void runTests(int n) {
+        long stringTime = testString(n);
+        long stringBuilderTime = testStringBuilder(n);
+
+        System.out.println("Time using String: " + stringTime + " ms");
+        System.out.println("Time using StringBuilder: " + stringBuilderTime + " ms");
+    }
+
+    // Method to test String concatenation
+    public static long testString(int n) {
+        long start = System.currentTimeMillis();
+        String str = "";
+        for (int i = 0; i < n; i++) {
+            str += "a";
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+
+    // Method to test StringBuilder concatenation
+    public static long testStringBuilder(int n) {
+        long start = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append("a");
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
     }
 }
+
+
+
+
 //
